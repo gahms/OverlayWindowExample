d4d9e0caf8f06ec31d418040e93fbd221ceaa8e5
Nicolai Henriksen <nih@miracle.dk>
2021-12-14 13:45:46 +0100

    Initial Commit

---
+++ OverlayWindowExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OverlayWindowExample
+//
+//  Created by Nicolai Henriksen on 14/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OverlayWindowExample/OverlayWindowExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlayWindowExampleApp.swift
+//  OverlayWindowExample
+//
+//  Created by Nicolai Henriksen on 14/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct OverlayWindowExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
