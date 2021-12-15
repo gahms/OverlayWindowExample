fdeb05ba1ddc39379671fe0d4e0a6129375028a9
Nicolai Henriksen <nih@miracle.dk>
2021-12-15 17:04:26 +0100

    Example body built out with extra line, more text and flush left and top.

--- OverlayWindowExample/OverlayExampleBody.swift
+++ OverlayWindowExample/OverlayExampleBody.swift
@@ -2,13 +2,25 @@ import SwiftUI
 
 struct OverlayExampleBody: View {
     var body: some View {
-        Text("Hello from SwiftUI")
-            .foregroundColor(.white)
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Hello from SwiftUI, here comes a very long line")
+                    .foregroundColor(.white)
+                Text("Second line")
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            Spacer()
+        }
+        .padding()
     }
 }
 
 struct OverlayExampleBody_Previews: PreviewProvider {
     static var previews: some View {
         OverlayExampleBody()
+            .background(Color.yellow.opacity(0.4))
+            .frame(width: 240, height: 148)
+            .background(Color.black)
     }
 }
