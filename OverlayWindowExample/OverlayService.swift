ee26504bb10f70f66e6a421a265d6e532e7f8c1b
Nicolai Henriksen <nih@miracle.dk>
2021-12-15 17:05:06 +0100

    Reset saved place and size on each launch. Set initial size bigger.

--- OverlayWindowExample/OverlayService.swift
+++ OverlayWindowExample/OverlayService.swift
@@ -4,6 +4,18 @@ import UIKit
 
 class OverlayService {
     func show() {
+        let prefKeys = [
+            "OverlayWindow_X",
+            "OverlayWindow_Y",
+            "OverlayWindow_Width",
+            "OverlayWindow_Height",
+        ]
+        for prefKey in prefKeys {
+            UserDefaults.standard.removeObject(forKey: prefKey)
+        }
+
+        OverlayWindowManager.shared.defaultOverlaySize
+        = .init(width: 350, height: 148)
         OverlayWindowManager.shared.setBody(view: OverlayExampleBody())
         OverlayWindowManager.shared.isVisible = true
     }
